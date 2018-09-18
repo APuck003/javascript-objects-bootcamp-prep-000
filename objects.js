32b1d855349b2ff5a3d03ab9ff5eca761152e20d
Austin Puckett <puckett_austin@yahoo.com>
2018-09-18 22:19:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {beatles: "Blackbird", hendrix: "Little Wing"};
 
 function updatePlaylist(obj, artist, song) {
-  playlist.artist = song;
+  playlist.[artist] = song;
   return playlist;
 }
+
